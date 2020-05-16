51425c215287e394fdd0840255f2affb9ae868c0
hirokidesuga <62142935+hirokidesuga@users.noreply.github.com>
2020-05-17 07:17:11 +0900

    Add description

--- rent_roomsize/answer_rent_by_roomsize.py
+++ rent_roomsize/answer_rent_by_roomsize.py
@@ -1,12 +1,19 @@
+# 必要な変数を対応ファイルから読み込み
 from rent_roomsize import a, df
 
 
+# 部屋の広さxを標準入力し、家賃y_hatを返す関数
 def predict(x):
     y_hat = a * (x - df.mean()['x']) + df.mean()['y']
     return y_hat
 
 
-print("部屋の広さxから家賃を予測できます(30 <= x <= 50, 単位：平米)")
-room_size_input = int(input("広さ："))
-output = "広さ" + str(room_size_input) + "平米のお部屋の家賃はおおよそ" + str(predict(room_size_input)) + "円です"
-print(output)
+def main():
+    print("部屋の広さxから家賃を予測できます(30 <= x <= 50, 単位：平米)")
+    room_size_input = int(input("広さ："))
+    output = "広さ" + str(room_size_input) + "平米のお部屋の家賃はおおよそ" + str(predict(room_size_input)) + "円です"
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
